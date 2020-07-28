5f91ef65ff67fc8f29e5153d10f01113e0458fcb
Sivaprakash Gorantla <sivaprakashgorantla@gmail.com>
2020-07-28 21:23:07 +0530

    initil commit

---
+++ springboot-exception/src/test/java/com/gorantla/exception/SpringbootExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorantla.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
